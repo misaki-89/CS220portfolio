8c3abbdd474d6f0c86aafcf7c1b60ce0063f49da
M K <may.key.2030638@gmail.com>
2026-04-14 18:38:39 -0600

    change

--- src/components/home/Skills.jsx
+++ src/components/home/Skills.jsx
@@ -1,5 +1,6 @@
+"use client";
+
 import Image from "next/image";
-("use client");
 
 export default function Skills() {
   const title = "Skills";
--- src/components/home/Work.jsx
+++ src/components/home/Work.jsx
@@ -1,5 +1,6 @@
+"use client";
+
 import Image from "next/image";
-("use client");
 
 export default function Work() {
   const title = "Work";
